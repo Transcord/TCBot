727e0ba927f3e468fafe79046b13f8a7b60f2d11
asheliahut <asheliahut@gmail.com>
2016-02-22 22:43:03 -0600

    Update to make callmod work with all client connections to any server. Yay for learning.

--- commands/callmod.js
+++ commands/callmod.js
@@ -1,10 +1,8 @@
-// test command to see if the bot works
+//Sends a message to a specific channel that only mods have access to. 
 
 var callmod = function(message) {
 	var config = require("../config/config.js");
-	var serverConfig = require("../config/server.js");
-	var server = message.client.servers.get("id", serverConfig.id);
-	var channel = server.channels.get("name", config.modChannelName);
+	var channel = message.channel; 
 	var lastMessage = message.author.name + " said: " + message.content.substring(9);
 	message.client.sendMessage(channel, lastMessage,{},function(){});	
 };
