b83f6ec6e930c81d3f3ca987a8060d8b73457a49
asheliahut <asheliahut@gmail.com>
2016-02-24 16:47:17 -0600

    Update callmod to send to transcord admin server.

--- commands/callmod.js
+++ commands/callmod.js
@@ -3,8 +3,8 @@
 var callmod = function(message) {
 	var config = require("../config/config.js");
 	var serverConfig = require("../config/server.js");
-	var server = message.client.servers.get("id", serverConfig.id);
-	var channel = server.channels.get("name", config.modChannelName);
+	var modServer = message.client.servers.get("id", serverConfig.modId);
+	var channel = modServer.channels.get("name", config.modChannelName);
 	var lastMessage = message.author.name + " said: " + message.content.substring(9);
 	message.client.sendMessage(channel, lastMessage,{},function(){});	
 };
