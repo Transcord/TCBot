34fae8dc22f134a537eef17cdc330fae8e22e9d0
asheliahut <asheliahut@gmail.com>
2016-03-03 16:37:52 -0600

    Update rick roll to run on SOA based code.

---
+++ services/voiceService.js
@@ -0,0 +1,19 @@
+var voiceService = {
+	playSong: function(message, fileToPlay, timeToPlay){
+		var roles = require('../config/roles.js');
+
+		if (!message.client.memberHasRole(message.author, roles["Staff"])) {
+        	        message.client.sendMessage(message.channel, "Only staff can troll here.");
+	        }else{
+        	        var voiceChannel = message.author.voiceChannel;
+	                if(voiceChannel !== null){
+                        	message.client.joinVoiceChannel(voiceChannel, function(err, connection){
+        	                        connection.playFile(fileToPlay);
+	                                setTimeout(function(){
+                                	        message.client.leaveVoiceChannel();
+                        	        }, timeToPlay);
+                	        });
+        	        }
+	        }
+	}
+};
