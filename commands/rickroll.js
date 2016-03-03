9e04a02159bfa5c4f7ae2933d521c2004551fcf1
asheliahut <asheliahut@gmail.com>
2016-03-03 15:11:40 -0600

    Update rick roll to dissconnect properly. and play the song and ignore afk channel.

--- commands/rickroll.js
+++ commands/rickroll.js
@@ -1,26 +1,34 @@
-// rick rolling command to see if the bot works
+// rick rolling command only for mods to troll 
 
 var rickroll = function(message) {
 	var roles = require('../config/roles.js');
-
+	var config = require('../config/config.js');
 
 	if (!message.client.memberHasRole(message.author, roles["Staff"])) {
     		message.client.sendMessage(message.channel, "Only staff can troll here.");
 	}else{
-		var voiceChannel = getMostMemberChannel(message.channel.server.channels.getAll("type", "voice"));
+		var voiceChannel = getMostMemberChannel(message.channel.server.channels.getAll("type", "voice"), config.afkChannelId);
 	        message.client.joinVoiceChannel(voiceChannel, function(err, connection){
                 	var fileToPlay = __dirname + '/../media/rickroll.mp3';
                 	connection.playFile(fileToPlay);
-                	connnection.destroy();
+			setTimeout(function(){
+				connection.destroy();
+			}, 8000);
         	});
 
 	}
 };
 
 
-var getMostMemberChannel = function(voiceChannels) {
+var getMostMemberChannel = function(voiceChannels, afkChannelId) {
 	var maxChannelLength = 0,
         maxChannel = null;
+	//Remove AFK Channel From voiceChannels
+	afkChannelId = afkChannelId || null;
+	console.log(afkChannelId);
+	if(afkChannelId !== null){
+		voiceChannels.remove(afkChannelId);
+	}
 	//Loop through voice channels to determine largest room
         for(var length = voiceChannels.length, i = 0; i < length; i++){
                 if(voiceChannels[i].members.length > maxChannelLength) {
