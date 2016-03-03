9bf7899f8198b1d0daa994bac07927734e935c98
asheliahut <asheliahut@gmail.com>
2016-03-03 16:55:05 -0600

    update error in export of hello.

--- commands/hello.js
+++ commands/hello.js
@@ -1,9 +1,9 @@
-// rick rolling command only for mods to troll 
+// Adele Hello command only for mods to troll 
 
-var rickroll = function(message) {
+var hello = function(message) {
 	var fileToPlay = __dirname + '/../media/hello.mp3';
 	var voiceService = require('../services/voiceService.js');
 	voiceService.playSong(message, fileToPlay, 6000);
 };
 
-module.exports = rickroll;
+module.exports = hello;
