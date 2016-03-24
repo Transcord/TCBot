0e329f28bdabd3ddf148e62912b39d46d15d2be9
asheliahut <asheliahut@gmail.com>
2016-03-23 19:25:21 -0500

    remove error

--- services/roleBlaster.js
+++ services/roleBlaster.js
@@ -6,7 +6,7 @@ var roleBlaster = {
 	genRoles: /\sstraight|\sgay|\slesbian|\sbi|\span|\sace|\sdemi|\spoly|\squeer|\sshe|\she|\sthey|\sxe/ig,
 	transStatus: /\smtf|\sftm|\sally/i,
 	rolesList: /\smale|\sfemale|\sgenderfluid|\snonbinary|\squestioning|\sstraight|\sgay|\slesbian|\sbi|\span|\sace|\sdemi|\spoly|\squeer|\sshe|\she|\sthey|\sxe|\smtf|\sftm|\sally|\sMember|\sSupport/ig,
-	normalize: this.R.compose(this.R.toLower, this.R.trim),s
+	normalize: this.R.compose(this.R.toLower, this.R.trim),
 	getRoleID: this.R.flip(R.prop)(this.roles),
 	normalizeToID: this.R.compose(this.getRoleID,this.normalize)
 };
