fe9faaaaadda658cbd9baa7669efc24a58293b94
asheliahut <asheliahut@gmail.com>
2016-06-25 20:55:07 -0500

    update hotlines command to have new lines.

--- commands/hotlines.js
+++ commands/hotlines.js
@@ -1,10 +1,10 @@
 // hotlines command
 
 var hotlines = function(message) {  
-	var messageToSend = "**100 Reasons to Not Kill Yourself**: <http://tinyurl.com/jyse52u>" +
-		"**International Suicide Helplines**: <http://www.suicide.org/international-suicide-hotlines.htm>l" +
-		"**The Trevor Project**: <http://www.thetrevorproject.org> | (866)-488-7386" +
-		"**Trans Lifeline: US**: (877)-565-8860 | Canada: (877)-330-6366";
+	var messageToSend = "**100 Reasons to Not Kill Yourself**: <http://tinyurl.com/jyse52u>\n\n" +
+		"**International Suicide Helplines**: <http://www.suicide.org/international-suicide-hotlines.html>\n" +
+		"**The Trevor Project**: <http://www.thetrevorproject.org> | (866)-488-7386\n" +
+		"**Trans Lifeline: US**: (877)-565-8860 | Canada: (877)-330-6366\n";
 	message.client.sendMessage(message.channel, messageToSend);
 };
 
