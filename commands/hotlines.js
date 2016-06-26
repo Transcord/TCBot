ec56abb25e91813699ed7d2a3711b78b16ccefd4
asheliahut <asheliahut@gmail.com>
2016-06-25 21:00:37 -0500

    update the us to not be bolded in hotlines.

--- commands/hotlines.js
+++ commands/hotlines.js
@@ -4,7 +4,7 @@ var hotlines = function(message) {
 	var messageToSend = "**100 Reasons to Not Kill Yourself**: <http://tinyurl.com/jyse52u>\n\n" +
 		"**International Suicide Helplines**: <http://www.suicide.org/international-suicide-hotlines.html>\n" +
 		"**The Trevor Project**: <http://www.thetrevorproject.org> | (866)-488-7386\n" +
-		"**Trans Lifeline: US**: (877)-565-8860 | Canada: (877)-330-6366\n";
+		"**Trans Lifeline**: US: (877)-565-8860 | Canada: (877)-330-6366\n";
 	message.client.sendMessage(message.channel, messageToSend);
 };
 
