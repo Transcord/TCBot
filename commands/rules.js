e99c07e5dae107f1ee527dc237b4054439f02426
asheliahut <asheliahut@gmail.com>
2016-07-14 21:59:47 -0500

    update rules command to be proper message output.

--- commands/rules.js
+++ commands/rules.js
@@ -1,7 +1,7 @@
 // rules command
 
 var rules = function(message) {  
-	var messageToSend = "**Rules**: <Rules: http://tinyurl.com/gsvx747>" 
+	var messageToSend = "**Rules**: <http://tinyurl.com/gsvx747>" 
 	message.client.sendMessage(message.channel, messageToSend);
 };
 
