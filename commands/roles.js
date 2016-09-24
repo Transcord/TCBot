a16e4fcd4683eb853b2a2cf233feb5443bf33b7a
asheliahut <asheliahut@gmail.com>
2016-09-23 22:30:05 -0500

    update roles command

--- commands/roles.js
+++ commands/roles.js
@@ -3,19 +3,20 @@
 var roles = function(message) {
     var userRoles = message.server.rolesOfUser(message.mentions[0]);
     var userRoleNames = [];
-    
+
     for(var role in userRoles){
         userRoleNames.push(userRoles[role].name);
     }
-    var messageToSend = 'The user mentioned has the following roles: ';
-    
+
+    var messageToSend = 'The user has the following roles: ';
+
     if(userRoleNames.length == 0){
-        messageToSend = 'The user mentioned has no roles.';    
+        messageToSend = 'The user has no roles.';    
     }
     if(userRoleNames.length == 1){
-        messageToSend = 'The user mentioned has this role: ';    
+        messageToSend = 'The user has this role: ';    
     }
-    message.client.sendMessage(channel, messageToSend + userRoleNames.join());
+    message.client.sendMessage(message, messageToSend + userRoleNames.join());
 
 };
 
