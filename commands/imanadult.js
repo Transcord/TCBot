944b46b38bb0fec484d7396c3493296afe8878f2
asheliahut <asheliahut@gmail.com>
2016-09-23 21:58:13 -0500

    adult checker now says wait 24 hours

--- commands/imanadult.js
+++ commands/imanadult.js
@@ -8,6 +8,8 @@ var imanadult = function(message) {
     
     if(tsYesterday >= createdAtTimestamp){
         message.client.sendMessage(message.author, 'https://discord.gg/0wDu0KUT8wxt3QlW');
+    } else{
+        message.client.sendMessage(message.author, 'You have not been a member for 24 hours yet, please try again later.');
     }
 };
 
