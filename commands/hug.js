d96aa2cff36f901480691cd0f71e2e8e3e6d9d2d
asheliahut <asheliahut@gmail.com>
2016-09-23 21:04:42 -0500

    update hug to not hug the donothug

--- commands/hug.js
+++ commands/hug.js
@@ -3,11 +3,24 @@
 var hug = function(message) {  
 	var mention = message.mentions[0];
 	var messageToSend = "*hugs* ";
+    
+    var roles = require('../config/roles.js');
+    var doNotHugRole = roles["donothug"];
+    
+    var usersToHug = [];
+    for(var user in message.mentions){
+        if (!message.client.memberHasRole(message.mentions[user], roles["donothug"])) {
+            usersToHug.push(message.mentions[user]);
+        }
+    }
+    messageToSend += usersToHug.join();
+    
 	if(mention == undefined){
 		var onlineUsers = message.client.users.getAll("status", "online");
-		messageToSend += onlineUsers[Math.floor(Math.random()*onlineUsers.length)];
-	} else{
-		messageToSend += message.mentions.join();
+        var userToHug = onlineUsers[Math.floor(Math.random()*onlineUsers.length)];
+        if (!message.client.memberHasRole(userToHug, roles["donothug"])) {
+            messageToSend += onlineUsers[Math.floor(Math.random()*onlineUsers.length)];
+        }
 	}
 	message.client.sendMessage(message.channel, messageToSend);
 };
